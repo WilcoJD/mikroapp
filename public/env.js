dcc1c202405a61953082655fcc7c4e1dd2d186a1
Sami <bassra@gmail.com>
2019-06-13 02:22:59 +0500

    config

--- public/env.js
+++ public/env.js
@@ -1,6 +1,6 @@
 window.env = {
     // This option can be retrieved in "src/History.js" with "window.env.API_URL".
-    API_URL: 'http://localhost:9001'
+    // API_URL: 'http://localhost:9001'
     // API_URL: 'http://www.coinxtrade.io'
-    // API_URL: 'http://www.app.local'
+    API_URL: 'http://www.app.local'
 };
